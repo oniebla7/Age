7f4422c1e8f8db62eebf1a4b19d3e49c03baf597
oniebla7 <oniebla7@gmail.com>
2015-03-04 00:16:40 -0800

    Initial Commit

---
+++ Text/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Text
+//
+//  Created by Oscar Fuentes on 3/4/15.
+//  Copyright (c) 2015 Oscar Fuentes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
