35868227f5ab20d2fa42415654ab62c511adac71
oniebla7 <oniebla7@gmail.com>
2015-03-05 11:45:57 -0800

    If statements
    If statements using age

--- Text/main.cpp
+++ Text/main.cpp
@@ -1,15 +1,43 @@
-//
-//  main.cpp
-//  Text
-//
-//  Created by Oscar Fuentes on 3/4/15.
-//  Copyright (c) 2015 Oscar Fuentes. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void printAge (int t, int g)
+{
+    cout << "Oscar is " << t << " years old\n";
+    cout << "Jimbob is " << g << " years old\n";
+}
+
+int main()
+{
+    printAge(19, 3);
+    int a, b;
+    
+   
+    cout << "\n Enter Oscar's age, bud\n" ;
+    cin >> a;
+    cout << "Go ahead and enter Jimbob's age\n";
+    cin >> b;
+    
+    
+    
+    if (a > b)
+    {
+     cout << "Oscar is older \n" ;
+    
+    }
+    if ( a < b)
+    {
+        cout << "Jimbob is older\n" ;
+    }
+    if ( a == b)
+    {
+        cout << "They're both just as old\n" ;
+        
+    }
+    
+
+    
     return 0;
+    
 }
